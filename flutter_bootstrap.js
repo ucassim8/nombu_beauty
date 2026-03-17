95dedbb2cd2beb4f48d2a3a7dd0051dc3a8df7b2
ucassim8 <ucassim8@users.noreply.github.com>
2026-03-17 20:24:31 +0000

    deploy: e752020f72c2a338586c6b2e82e722b45cd3fa3a

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"b8800d88be4866db1b15f8b954ab2573bba996
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1181955011"
+    serviceWorkerVersion: "1044596977"
   }
 });
