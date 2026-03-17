0f572c641a6de6090f1c0dc5de711df9a2ed22f8
ucassim8 <ucassim8@gmail.com>
2026-03-17 21:01:03 +0200

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -317,15 +317,7 @@ class _ServiceScreenState extends State<ServiceScreen> {
                   onChanged: (val) => setState(() => afterHours = val!),
                 ),
                 Text('After-hours (+R100)'),
-              ],
-            ),
-            SizedBox(height: 16),
-          ],
-        ),
-      ),
-    );
-  }
-}
+
 // ------------------------- SERVICE SCREEN -------------------------
 class ServiceScreen extends StatefulWidget {
   final String category;
